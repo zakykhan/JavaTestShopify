b9f35c518dc040e2bc8b9e38e4398d98b9b06c08
zaky <zakykhan47@gmail.com>
2020-12-15 00:51:50 +0530

    adding global exception handler

---
+++ src/main/java/com/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value
+            = {Exception.class, IllegalStateException.class}) // we can add more generic exceptions here
+    protected ResponseEntity<Object> handleConflict(
+            RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "Some problem while processing your request";
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
